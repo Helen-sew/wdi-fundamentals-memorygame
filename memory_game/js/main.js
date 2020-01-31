0d42bd1d79739614bd804e7765fedf3321b5c31e
helen sew <sew.helen@yahoo.com.sg>
2020-01-31 18:18:33 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "king";
-let cardFour = "queen";
+let cards = ['queen','queen','king','king'];
+let cardsInPlay =[];
+let cardOne = [cards[0]];
+cardsInPlay.push(cardOne);
 
-console.log('user flipped' + " " + cardOne);
+
+let cardTwo = [cards[2]];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" +" " + cardsInPlay[0]);
+console.log("User flipped" +" " + cardsInPlay[1]);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert('You found a match!');
+} else {
+  alert('Sorry, try again!');
+}
